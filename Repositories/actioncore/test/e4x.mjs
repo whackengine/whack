77813a489b0192c215056a00dc4b6958a0654bfd
hydroperfox <hydroperfox@gmail.com>
2024-08-20 13:29:03 -0300

    Update

--- Repositories/actioncore/test/e4x.mjs
+++ Repositories/actioncore/test/e4x.mjs
@@ -2,7 +2,7 @@ import * as $ from "../src/index.js";
 
 const xnode = $.construct($.xmlclass, `
 <?xml version="1.0"?>
-<a:data xmlns="a" val="10">
+<a:data xmlns:a="a" val="10">
     <a:item/>
 </a:data>
 `);
@@ -10,5 +10,5 @@ const xnode = $.construct($.xmlclass, `
 const a_ns = $.construct($.namespaceclass, "a", "a");
 
 const datael = $.getproperty($.getproperty(xnode, a_ns, "data"), null, 0);
-console.log(datael)
-console.log($.getattribute(datael, null, "val"));
+console.log("<a:data/>.nodeKind() =", $.callproperty(datael, null, "nodeKind"));
+console.log("<a:data/>.@val =", $.getattribute(datael, null, "val"));
