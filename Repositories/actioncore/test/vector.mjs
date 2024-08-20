547ef1762519e6569c163e45e8c8a4c8bf50a503
hydroperfox <hydroperfox@gmail.com>
2024-08-20 13:44:10 -0300

    Update

--- Repositories/actioncore/test/vector.mjs
+++ Repositories/actioncore/test/vector.mjs
@@ -1,8 +1,13 @@
 import * as $ from "../src/index.js";
 
 const list = $.construct($.vectorfloatclass);
-$.callproperty(list, null, "push", 10.5);
-$.postincrementproperty(list, null, 0);
+
+console.log("const list = new <float>[];");
+
+console.log("list.push(10.5) =", $.callproperty(list, null, "push", 10.5));
+
+// 10.5
+console.log("list[0]++ =", $.postincrementproperty(list, null, 0));
 
 // 11.5
 console.log("list[0] =", $.getproperty(list, null, 0));
